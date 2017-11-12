e321484a13aaa3f9457285a7e83e6ebd1ab68510
ines-vermeir <ines.vermeir@student.ehb.be>
2017-11-12 13:58:04 +0100

    functies in klasse main plaatsen

--- src/db/LocationDAO.java
+++ src/db/LocationDAO.java
@@ -25,7 +25,7 @@ public class LocationDAO extends BaseDAO{
 		return l;
 	}
 	
-	public ArrayList<Location> getAllLocations() throws SQLException, Exception{
+	public static ArrayList<Location> getAllLocations() throws SQLException, Exception{
 		
 		Statement stm = null; 
 		ResultSet r = null; 
@@ -60,7 +60,7 @@ public class LocationDAO extends BaseDAO{
 			}
 		}
 		}
-	public Location getLocationsById(int id) throws SQLException, Exception{
+	public static Location getLocationById(int id) throws SQLException, Exception{
 		 
 		ResultSet r = null; 
 		PreparedStatement p = null; 
@@ -92,7 +92,7 @@ public class LocationDAO extends BaseDAO{
 		}
 		}
 	
-	public boolean deleteLocation(int id) throws SQLException, Exception{
+	public static boolean deleteLocation(int id) throws SQLException, Exception{
 		
 		boolean successvol = false; 
 		PreparedStatement p = null; 
@@ -123,7 +123,7 @@ public class LocationDAO extends BaseDAO{
 		
 	}
 
-	public boolean insertLocation(Location l) throws SQLException, Exception{
+	public static boolean insertLocation(Location l) throws SQLException, Exception{
 		
 		boolean successvol = false; 
 		PreparedStatement p = null; 
@@ -160,7 +160,7 @@ public class LocationDAO extends BaseDAO{
 		
 	}
 
-	public boolean updateLocation(Location l) throws SQLException, Exception{
+	public static boolean updateLocation(Location l) throws SQLException, Exception{
 		
 		boolean successvol = false; 
 		PreparedStatement p = null; 
--- src/logic/Main.java
+++ src/logic/Main.java
@@ -9,21 +9,27 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import db.BookDAO;
-import db.BookDB;
-import db.TestGson;
+import db.LocationDAO;
+/*import db.TestGson;
 import db.TestJackson;
 import db.UserDB;
-import logic.User.Privilege;
+import logic.User.Privilege;*/
 
 import java.util.Date;
 import db.SessionDAO;
-import db.SurveyDAO;
+/*import db.SurveyDAO;*/
 
 public class Main {
 
 	/*
 	 * LOGIN 
 	 * 
+	 */
+	public void login () throws IOException {
+		menuEmployee(1, "Tim");
+	}
+	/*
+	 * end LOGIN
 	 */
 	
 	/* MENU PRIVILEGE 1 (EMPLOYEE)  */
@@ -45,10 +51,10 @@ public class Main {
 		 		break;
 		case 2: /*certificateMenu(privilege)*/
 				break;
-		case 3: return;	
+		case 3: break;	
 		default: break;
-			
 		}
+		return;
 	}
 	
 	
@@ -85,10 +91,271 @@ public class Main {
 	 * 
 	 */
 	
+	/*
+	 * Location
+	 * 
+	 */
+	
+	/*
+	 * CHANGE LOCATION FUNCTIONS
+	 */
+	public void changeStreetName (int id) throws SQLException, Exception {
+		Location l = new Location(LocationDAO.getLocationById(id));
+		System.out.println("What is the new streetName: ");
+		String input;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	    try {
+			input = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		System.out.println(input);
+		if (saveUpdate() ==  true) {
+			l.streetName = input;
+			if (LocationDAO.updateLocation(l) == true) {
+				System.out.println("UPDATE SUCCESFULL");
+			}
+			else {
+				System.out.println("ERROR: UPDATE UNSUCCESFULL");
+			}
+		}
+		else {
+			System.out.println("CHANGES DELETED");
+		}
+		return;
+	}
+	public void changeNumber (int id) throws SQLException, Exception { 
+		Location l = new Location(LocationDAO.getLocationById(id));
+		System.out.println("What is the new number: ");
+		String input;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	    try {
+			input = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		System.out.println(input);
+		if (saveUpdate() ==  true) {
+			l.number = input;
+			if (LocationDAO.updateLocation(l) == true) {
+				System.out.println("UPDATE SUCCESFULL");
+			}
+			else {
+				System.out.println("ERROR: UPDATE UNSUCCESFULL");
+			}
+		}
+		else {
+			System.out.println("CHANGES DELETED");
+		}
+		return;
+	}
+	public void changePostalCode (int id) throws SQLException, Exception { 
+		Location l = new Location(LocationDAO.getLocationById(id));
+		System.out.println("What is the new postal code: ");
+		String input;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	    try {
+			input = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		System.out.println(input);
+		if (saveUpdate() ==  true) {
+			l.postalCode = input;
+			if (LocationDAO.updateLocation(l) == true) {
+				System.out.println("UPDATE SUCCESFULL");
+			}
+			else {
+				System.out.println("ERROR: UPDATE UNSUCCESFULL");
+			}
+		}
+		else {
+			System.out.println("CHANGES DELETED");
+		}
+		return;
+	}
+	public void changeCity (int id) throws SQLException, Exception {
+		Location l = new Location(LocationDAO.getLocationById(id));
+		System.out.println("What is the new city: ");
+		String input;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	    try {
+			input = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+	    System.out.println(input);
+		if (saveUpdate() ==  true) {
+			l.postalCode = input;
+			if (LocationDAO.updateLocation(l) == true) {
+				System.out.println("UPDATE SUCCESFULL");
+			}
+			else {
+				System.out.println("ERROR: UPDATE UNSUCCESFULL");
+			}
+		}
+		else {
+			System.out.println("CHANGES DELETED");
+		}
+		return;
+	}
+	public void changeCountry (int id) throws SQLException, Exception {
+		Location l = new Location(LocationDAO.getLocationById(id));
+		System.out.println("What is the new country: ");
+		String input;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	    try {
+			input = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+	    System.out.println(input);
+		if (saveUpdate() ==  true) {
+			l.postalCode = input;
+			if (LocationDAO.updateLocation(l) == true) {
+				System.out.println("UPDATE SUCCESFULL");
+			}
+			else {
+				System.out.println("ERROR: UPDATE UNSUCCESFULL");
+			}
+		}
+		else {
+			System.out.println("CHANGES DELETED");
+		}
+		return;
+	}
+	public void changeName (int id) throws SQLException, Exception {
+		Location l = new Location(LocationDAO.getLocationById(id));
+		System.out.println("What is the new name: ");
+		String input;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	    try {
+			input = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+	    System.out.println(input);
+		if (saveUpdate() ==  true) {
+			l.postalCode = input;
+			if (LocationDAO.updateLocation(l) == true) {
+				System.out.println("UPDATE SUCCESFULL");
+			}
+			else {
+				System.out.println("ERROR: UPDATE UNSUCCESFULL");
+			}
+		}
+		else {
+			System.out.println("CHANGES DELETED");
+		}
+		return;
+	}
+	public void changeInfo (int id) throws SQLException, Exception {
+		Location l = new Location(LocationDAO.getLocationById(id));
+		System.out.println("What is the new info: ");
+		String input;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	    try {
+			input = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+	    System.out.println(input);
+		if (saveUpdate() ==  true) {
+			l.postalCode = input;
+			if (LocationDAO.updateLocation(l) == true) {
+				System.out.println("UPDATE SUCCESFULL");
+			}
+			else {
+				System.out.println("ERROR: UPDATE UNSUCCESFULL");
+			}
+		}
+		else {
+			System.out.println("CHANGES DELETED");
+		}
+		return;
+	}
+
+	/* 
+	 * end CHANGE LOCATION FUNCTIONS
+	 */
+	public void deleteLocation() {
+		/* locatie verwijderen vanuit sessie klasse en locationDAO???*/
+	}
+	
+	public void addLocation() throws SQLException, Exception {
+		String[] questions = new String[]{"What is the streetName: ","What is the number: ", "What is the new postal code: ", "What is the city: ", "What is the country: ","What is the name: ","What is the info: " };
+		String[] input = new String[questions.length];
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		for (int i=0; i < questions.length; i++) {
+	    try {
+	    	System.out.println(questions[i]);
+			input[i] = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		}
+	    Location l = new Location (-1, input[0], input[1], input[2],input[3],input[4],input[5], input[6]);
+	    System.out.println(l.toString());
+	    if (saveUpdate() ==  true) {
+			if ( LocationDAO.insertLocation(l) == true) {
+				System.out.println("INSERT SUCCESFULL");
+			}
+			else {
+				System.out.println("ERROR: INSERT UNSUCCESFULL");
+			}
+		}
+		else {
+			System.out.println("INSERT DELETED");
+		}
+	    return;
+	}
+	/*
+	 * end LOCATION
+	 * 
+	 */
+	
+	
+	/*
+	 * SAVE methode
+	 * 
+	 */
+	public boolean saveUpdate() {
+		String input = null;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		do {
+		System.out.println("Save(Y/N): ");
+	    try {
+			input = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		input = input.toUpperCase();
+		}while (!input.equals("Y") && !input.equals("N"));
+		
+		switch (input) {
+		case "Y":  return true;
+		case "N": return false;
+		default: return false;
+		}
+	}
+	/*
+	 * end SAVE
+	 * 
+	 */
+	
+	
 	public static void main(String[] args) throws SQLException, Exception {
 		
 		System.out.println("Welkom");
-
+		Main m = new Main();
+		m.login();
 		
 		
 		
