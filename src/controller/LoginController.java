5b1c6b80fb7bbd94e41dc75bda38b26cb16c57fb
EvaBouton <eva.bouton@student.ehb.be>
2017-12-19 13:20:29 +0100

    code van Michiel: login

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,25 +1,74 @@
 package controller;
-
 import java.net.URL;
 import java.util.ResourceBundle;
-
+import org.mindrot.jbcrypt.BCrypt;
 import application.Navigator;
+import db.UserDB;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import logic.User;
 public class LoginController implements Initializable{
-	
-		//btnLogin Action
-		@FXML protected void doLogin(ActionEvent e) {
-			Navigator.loadVista(Navigator.HomeView);
-			Navigator.loadUserVista(Navigator.UserSectionView);
-			Navigator.loadMenuVista(Navigator.MenuHomeActiveView);
-		}
-
-		@Override
-		public void initialize(URL location, ResourceBundle resources) {
-			// TODO Auto-generated method stub
-			
-		}
+    
+    
+    @FXML
+    private TextField userField; 
+    
+    @FXML 
+    private TextField passwordField;
+    
+    @FXML
+    private Label lblStatus;
+    
+    
+    
+    
+    @SuppressWarnings("unused")
+    @FXML protected void doLogin(ActionEvent e) {
+        
+        
+        
+        boolean login = false;
+        boolean usernameGevonden = true;
+         
+        User user = null;
+        UserDB userDB = new UserDB();
+        
+        user = userDB.getUser(userField.getText());
+        
+        if (user == null) {
+            usernameGevonden = false;
+            lblStatus.setText("user not found");
+            lblStatus.setVisible(true);    
+            userField.setStyle("-fx-background-color: red;");
+        }
+        
+        String password = passwordField.getText();
+        
+        // 
+        if(userField.getText().equals(user.getUsername()) && BCrypt.checkpw(password,user.getPassword())) {
+            login = true;
+            System.out.println("welcome");
+            userField.setStyle("-fx-background-color: green;");
+        }
+        
+        if(login == true) {
+            Navigator.loadVista(Navigator.HomeView);
+            Navigator.loadUserVista(Navigator.UserSectionView);
+            Navigator.loadMenuVista(Navigator.MenuHomeActiveView);        
+        }
+        
+        if (usernameGevonden == true) {
+            lblStatus.setText("wrong password");
+            lblStatus.setVisible(true);    
+            userField.setStyle("-fx-background-color: orange;");
+        }
+    }
+        @Override
+        public void initialize(URL location, ResourceBundle resources) {
+            // TODO Auto-generated method stub
+            
+        }
 }
